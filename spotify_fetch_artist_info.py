bd9697309a7c3db643d068ca55fa6fe707999a43
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-10-22 12:16:27 +0300

    add script for uploadint tracks top

--- spotify_fetch_artist_info.py
+++ spotify_fetch_artist_info.py
@@ -104,12 +104,3 @@ result_artist_info_df.to_csv("artist_info.csv")
 st.subheader("Кол-во артистов, по которым смогли найти что-то в поиске")
 st.write( len(result_artist_info_df[ result_artist_info_df['followers'] > 0 ]) )
 
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
-
-# # Some number in the range 0-23
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-# st.map(filtered_data)
